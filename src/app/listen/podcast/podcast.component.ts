4e43a7bd62f772d990accef1834461f459919c9d
Trevor Sargent <trevor.a.sargent@gmail.com>
2021-11-01 16:11:23 -0700

    add podcast ep 3

--- src/app/listen/podcast/podcast.component.ts
+++ src/app/listen/podcast/podcast.component.ts
@@ -11,17 +11,23 @@ export class PodcastComponent implements OnInit {
       link: 'https://www.spreaker.com/user/15488677/episode-1-immigration_1',
       transcript:
         'https://docs.google.com/document/d/1hZpT0BWOiGE3kJtPVpks8PGVrHvCSLiC0Y7Zvf8t-34',
-      title: 'Episode 1',
+      title: '1: Immigration',
       isReleased: true,
     },
     {
       link: 'https://www.spreaker.com/user/15488677/episode-2-race-and-identity',
       transcript:
         'https://docs.google.com/document/d/1Awz-DfIh5VLR-XGVsWYk9f0Ygokl1XApHblZxLQE_4Q',
-      title: 'Episode 2',
+      title: '2: Race and Identity',
       isReleased: true,
     },
-    { link: '', transcript: '', title: 'Episode 3', isReleased: false },
+    {
+      link: 'https://www.spreaker.com/user/15488677/episode-3-23-and-me',
+      transcript:
+        'https://docs.google.com/document/d/1Y1Aufe9Ji5SGxtY7DwwCGRRDr0FO6PymOOgxUmPDu_8',
+      title: '3: 23 and Me',
+      isReleased: true,
+    },
     { link: '', transcript: '', title: 'Episode 4', isReleased: false },
   ];
 
